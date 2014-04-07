4fb840a14ea6158ee602327003489d8678f65d89
taliesin <terracoda@gmail.com>
2014-04-06 22:29:52 -0230

    Deleted unused js

--- sample-course/assets/js/munCopyright.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$("<div id='footer'><div class='copyright'><p>&copy; Memorial University of Newfoundland, 2013. All rights reserved.</p></div></div><!-- END footer -->").insertAfter("#contentContainer");
-});
